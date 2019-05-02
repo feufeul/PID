3fd528ff7c9b37c923df36f25b7d1f3c632bddb3
feufeul <vanduffelchristopher@gmail.com>
2019-05-02 19:08:07 +0200

    Show view up

--- PID/manager.py
+++ PID/manager.py
@@ -1,23 +1,27 @@
-from django.db import models
+from . import models
+#from django import models
 
 
-class UserManager(models.Manager):
+class UserManager():
     def all_user(self):
         from django.db import connection
         with connection.cursor() as cursor:
-            cursor.execute("""SELECT login FROM pid_user""")
+            cursor.execute("""SELECT login FROM PID_user""")
             result_list = []
             for row in cursor.fetchall():
                 result_list.append(row[0])
         return result_list
 
 
-class ShowManager(models.Manager):
+class ShowManager():
     def all_show(self):
+        result_list = models.Show.objects.all()
         from django.db import connection
-        with connection.cursor() as cursor:
-            cursor.execute("""SELECT title, price, bookable FROM pid_show""")
-            result_list = []
-            for row in cursor.fetchall():
-                result_list.append(row)
+ #       with connection.cursor() as cursor:
+  #          cursor.execute("""SELECT title, price, bookable FROM pid_show""")
+   #         result_list = []
+    #        for row in cursor.fetchall():
+     #           result_list.append(row)
+        for i in result_list:
+            print(i.login)
         return result_list
--- PID/views.py
+++ PID/views.py
@@ -1,6 +1,5 @@
 from django.views.generic import TemplateView
-from django.http import HttpResponse
-from . import manager
+from . import manager, models
 from django.shortcuts import render
 
 
@@ -9,40 +8,16 @@ class HomePageView(TemplateView):
 
 
 def get_users_name(request):
-    user_manager = manager.UserManager()
-    list = []
-    for e in user_manager.all_user():
-        list.append(e)
+    user_manager = list(models.User.objects.all())
     context = {
-        'list': list
+        'list': user_manager
     }
     return render(request, 'PID/users.html', context)
 
 
 def get_shows(request):
-    show_manager = manager.ShowManager().all_show()
-    info = {}
-    i = 1
-    for item in show_manager:
-        info.update({i: item})
-        i += 1
-    # context = {i: show_manager[i] for i in range(0, len(show_manager))}
-    # shows = []
-    # urls = []
-    # prices = []
-    # bookables = []
-    # for e in show_manager:
-    #     shows.append(e[1])
-    #     urls.append(e[2])
-    #     prices.append(e[3])
-    #     bookables.append(e[4])
-    # context = {
-    #     'shows': shows,
-    #     'urls': urls,
-    #     'prices': prices,
-    #     'bookables': bookables
-    # }
+    show_manager = list(models.Show.objects.all())
     context ={
-        "info": info
+        "info": show_manager
     }
     return render(request, 'PID/shows.html', context)
